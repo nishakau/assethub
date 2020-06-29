89bd3edb156925fef5658b8605a01e4fbc0d41eb
nishakau <nishant.k.kaushik@oracle.com>
2020-06-29 23:25:07 +0530

    edited path

--- startup/redeploybackendand.sh
+++ startup/redeploybackendand.sh
@@ -1,2 +1,2 @@
 pm2 stop assethubbackend
-pm2 start /u01/ahweb/backend/bin/assethubbackend --watch
+pm2 start /u03/ahweb/assethub/backend/bin/assethubbackend --watch
--- startup/startup.sh
+++ startup/startup.sh
@@ -1,5 +1,5 @@
 pm2 stop assethubbackend
-pm2 start /u01/ahweb/backend/bin/assethubbackend --watch
+pm2 start /u03/ahweb/assethub/backend/bin/assethubbackend --watch
 echo "node backend restarted . . ."
 sudo systemctl restart httpd
 echo "Apache Service restarted . . ."
